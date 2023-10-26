49fbb623f02d1f3b3ea9f8d7afc76e6886b4f604
wirdana <77711997+wirdana@users.noreply.github.com>
2023-10-27 07:26:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -110,8 +110,8 @@ for z in range(200):
 #[FBAN/MessengerLite;FBAV/{versi_chrome};FBBV/193013937;FBDM/"+"{density=2.625,width=1080,height=1794};"+f"FBLC/en_US;FBRV/0;FBCR/Verizon;FBMF/Google;FBBD/google;FBPN/com.facebook.mlite;FBDV/Pixel 2;FBSV/{versi_android};FBBK/1;FBOP/1;FBCA/arm64-v8a:;
 #FBDM/"+"{density=1.5,width=540,height=960};"+"FBLC/en_US;FBRV/183119516;FBCR/TM;FBMF/vivo;FBBD/vivo;FBPN/com.facebook.mlite;FBDV/vivo 1606;FBSV/{versi_android};FBOP/1;FBCA/armeabi-v7a:armeabi;]"
 #[FBAN/MessengerLite;FBAV/{versi_chrome};FBPN/com.facebook.mlite;FBLC/en_US;FBBV/{versi_app};FBCR/Airtel;FBMF/Facebook/lge;FBBD/FEVER;FBDV/FEVER;FBSV/{versi_android};FBCA/armeabi-v7a:armeabi;FBDM/"+"{density=2.75,width=1080,height=2179};FB_FW/1;])"
-	if ua in ugent:pass
-	else:ugent.append(ua)
+	#if ua in ugent:pass
+	#else:ugent.append(ua)
 	
 ###----------[ LOGO AUTHOR DAN VERSI]---------- ###
 class Logo:
